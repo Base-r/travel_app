469513baac6b68204bad5416ee9df1501a18c525
Base_R <lollymooll@gmail.com>
2023-09-18 13:22:59 +0300

    upload project

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:travel_olimp/ui/main_page.dart';
+
+void main() => runApp(MaterialApp(
+      home: MainPage(),
+    ));
---
+++ lib/ui/main_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:travel_olimp/ui/lnd/land_screen.dart';
+import 'package:travel_olimp/ui/map/map_screen.dart';
+import 'package:travel_olimp/ui/profile/profile.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          backgroundColor: Color.fromARGB(255, 113, 255, 161),
+          selectedIndex: currentPageIndex,
+          destinations: <Widget>[
+            NavigationDestination(
+              selectedIcon: Icon(
+                Icons.star,
+                color: Colors.black,
+              ),
+              icon: Icon(
+                Icons.star_outlined,
+                color: Colors.white,
+              ),
+              label: '',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(
+                Icons.chat,
+                color: Colors.black,
+              ),
+              icon: Icon(
+                Icons.chat_outlined,
+                color: Colors.white,
+              ),
+              label: '',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(
+                Icons.account_circle,
+                color: Colors.black,
+              ),
+              icon: Icon(
+                Icons.account_circle_outlined,
+                color: Colors.white,
+              ),
+              label: '',
+            )
+          ],
+        ),
+        body: <Widget>[
+          LandPage(),
+          MapPage(),
+          ProfilePage(),
+        ][currentPageIndex]);
+  }
+}
