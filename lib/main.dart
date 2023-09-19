c220c0bc2a3a8aed16084c66e43468e38dd9765f
Moon_Star32 <azamat.yusupov.04@outlook.com>
2023-09-19 03:24:52 +0300

    authorization_ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:travel_olimp/ui/main_page.dart';
+import 'package:travel_olimp/ui/authorization/auth_screen.dart';
 
-//haha
 void main() => runApp(MaterialApp(
-      home: MainPage(),
+      home: AuthScreen(),
     ));
