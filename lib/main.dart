412376669980a58de4bb6255b7c99b3f693f071c
Base_R <lollymooll@gmail.com>
2023-09-21 23:31:13 +0300

    the day of the end

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:travel_olimp/ui/authorization/auth_screen.dart';
 
-void main() => runApp(MaterialApp(
-      home: AuthScreen(),
+void main() => runApp(const MaterialApp(
+      home: MyApp(),
     ));
+    
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
